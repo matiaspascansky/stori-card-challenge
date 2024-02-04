e09a02e00d98b4e1e7315cf73220de00c69ff212
matiaspascansky <matias.pascansky@gmail.com>
2024-02-04 16:25:56 -0300

    integration simple test for api-gateway

--- cmd/main.go
+++ cmd/main.go
@@ -10,8 +10,4 @@ func main() {
 	// Register Lambda handlers
 	lambda.Start(handler.HandleAPIGatewayProxyRequest)
 
-	// For local testing with a health check endpoint
-	//http.HandleFunc("/health", handler.HealthCheckHandler)
-	//fmt.Println("Health check server started on :8080")
-	//http.ListenAndServe(":8080", nil)
 }
